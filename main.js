8075c8f59c91f670d2523f3c904c2a32fd2ca150
Mark Laurence Castillo <yo25365648@gmail.com>
2023-12-09 21:15:09 +0800

    adjgogwer5

--- main.js
+++ main.js
@@ -1,61 +1,51 @@
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let model;
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
-// Access the webcam and stream video
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-        video.srcObject = stream;
-    })
-    .catch((error) => {
-        console.error('Error accessing the webcam: ', error);
-    });
+// the link to your model provided by Teachable Machine export panel
+const URL = "./tm-my-image-model/";
 
-// Load the machine learning model
-async function loadModel() {
-    try {
-        model = await tf.loadLayersModel('tm-my-image-model/model.json');
-        console.log('Model loaded successfully.');
-    } catch (error) {
-        console.error('Error loading the model: ', error);
+let model, webcam, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
     }
 }
 
-// Event listener to perform actions on each frame
-video.addEventListener('play', () => {
-    loadModel(); // Load the model before processing frames
-
-    const processFrame = () => {
-        ctx.drawImage(video, 0, 0, 640, 480);
-
-        // Extract image data or use the canvas directly for prediction
-        const imageData = ctx.getImageData(0, 0, 640, 480);
-        const prediction = runModel(imageData); // Call your model prediction function
-
-        // Display or use the prediction results as needed
-        console.log('Prediction: ', prediction);
-
-        // Repeat the process for the next frame
-        requestAnimationFrame(processFrame);
-    };
-
-    processFrame();
-});
-
-// Function to run the model prediction
-function runModel(imageData) {
-    // Check if the model is loaded
-    if (model) {
-        // Implement your model prediction logic here
-        // Use the imageData or convert it to a suitable format for your model
-        const result = model.predict(convertImageDataToTensor(imageData));
-        // Return the prediction result
-        return result;
-    } else {
-        console.warn('Model not yet loaded. Waiting for model to load...');
-        return null; // or handle accordingly
-    }
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
 }
 
-// Additional functions as needed
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+}
