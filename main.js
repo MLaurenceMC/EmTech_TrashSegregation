0891f19b92e86cc034b7b3e05394d3ded53fd1a3
Mark Laurence Castillo <yo25365648@gmail.com>
2023-12-09 21:03:28 +0800

    adjgogwer3

---
+++ main.js
@@ -0,0 +1,61 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let model;
+
+// Access the webcam and stream video
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+        video.srcObject = stream;
+    })
+    .catch((error) => {
+        console.error('Error accessing the webcam: ', error);
+    });
+
+// Load the machine learning model
+async function loadModel() {
+    try {
+        model = await tf.loadLayersModel('tm-my-image-model/model.json');
+        console.log('Model loaded successfully.');
+    } catch (error) {
+        console.error('Error loading the model: ', error);
+    }
+}
+
+// Event listener to perform actions on each frame
+video.addEventListener('play', () => {
+    loadModel(); // Load the model before processing frames
+
+    const processFrame = () => {
+        ctx.drawImage(video, 0, 0, 640, 480);
+
+        // Extract image data or use the canvas directly for prediction
+        const imageData = ctx.getImageData(0, 0, 640, 480);
+        const prediction = runModel(imageData); // Call your model prediction function
+
+        // Display or use the prediction results as needed
+        console.log('Prediction: ', prediction);
+
+        // Repeat the process for the next frame
+        requestAnimationFrame(processFrame);
+    };
+
+    processFrame();
+});
+
+// Function to run the model prediction
+function runModel(imageData) {
+    // Check if the model is loaded
+    if (model) {
+        // Implement your model prediction logic here
+        // Use the imageData or convert it to a suitable format for your model
+        // Example: const result = model.predict(convertImageDataToTensor(imageData));
+        // Return the prediction result
+        return result;
+    } else {
+        console.warn('Model not yet loaded. Waiting for model to load...');
+        return null; // or handle accordingly
+    }
+}
+
+// Additional functions as needed
